d939b18aaffee85ba5936c2277085e8d23504c6c
Sangwon Park <wkqkel@naver.com>
2024-01-27 00:52:20 +0900

    [Bronze II] Title: 소수 찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+bool is_prime(int n){
+    if(n <= 1) return false;
+    for(int i = 2; i<n;i++){
+        if(n % i ==0) return false;
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int res = 0;
+    for(int i = 0; i < n; i++){
+        int v;
+        cin >> v;
+        if(is_prime(v)) res++;
+    }
+    
+    cout << res;
+}
