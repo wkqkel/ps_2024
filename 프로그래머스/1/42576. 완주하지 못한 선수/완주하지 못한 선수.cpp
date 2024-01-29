b0d4a0eed9eb3d33fdde2ba98a0bfe8c76421de5
Sangwon Park <wkqkel@naver.com>
2024-01-30 01:21:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 78.24 ms, Memory: 25.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    sort(completion.begin(), completion.end());
+    sort(participant.begin(), participant.end());
+    for(int i = 0; i < completion.size(); i++){
+        if(completion[i] != participant[i]) return participant[i];
+    }
+    return participant[participant.size()-1];
+}
