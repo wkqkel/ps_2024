595520efa1f1cc8c27e299242352c99499fe8e03
Sangwon Park <wkqkel@naver.com>
2024-01-30 18:12:22 +0900

    [Gold IV] Title: 공유기 설치, Time: 132 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, c;
+int arr[200020];
+
+bool solve(int s){
+    // 거리가 n이상이게 c개 설치할 수 있는지
+    int cnt = 1;
+    int prev = arr[0];
+    
+    for(int i = 1; i < n; i++){
+        if(arr[i] - prev >= s){
+            cnt++;
+            prev = arr[i];
+        }
+    }
+    
+    return cnt >= c;
+}
+
+int main()
+{
+
+    cin >> n >> c;
+    for(int i = 0; i < n;i++) cin >> arr[i];
+    
+    sort(arr, arr+n);
+    
+    int l = 1;
+    int r = 1e9;
+    
+    while(l <= r){
+        int mid = (l+r) / 2;
+        if(solve(mid)) l = mid + 1;
+        else r = mid - 1;
+    }
+    
+    cout << r;
+    return 0;
+}
