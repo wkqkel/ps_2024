5ba94ae10665bb67cb29f2227248503431b2ed76
Sangwon Park <wkqkel@naver.com>
2024-02-02 19:03:02 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,21 +1,22 @@
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
-int n;
-int dp[100020];
-int arr[100020];
-
+int arr[1001];
+int d[1001];
 int main()
 {
-  
+    int n;
     cin >> n;
+    
     for(int i = 1; i <= n; i++) cin >> arr[i];
     
-
-    for(int i = 1; i <= n; i++) {
-        dp[i] = max(dp[i-2], arr[i-1] + dp[i-3]) + arr[i];
+    d[1] = arr[1];
+    d[2] = arr[1] + arr[2];
+    for(int i = 3; i <= n; i++){
+        d[i] = max(d[i-2], arr[i-1] + d[i-3]) + arr[i];
     }
-    
-    cout << dp[n];
+   
+    cout << d[n];
+    return 0;
 }
