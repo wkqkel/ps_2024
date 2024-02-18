a62903ae9c3d33d7109b1221b8e0fda11fbf2e11
Sangwon Park <wkqkel@naver.com>
2024-02-19 01:02:11 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,22 +1,24 @@
-WITH value AS (
-    SELECT car.daily_fee, car.car_type, his.history_id,
-           DATEDIFF(end_date, start_date) + 1 AS period,
-    CASE 
-      WHEN DATEDIFF(end_date, start_date) + 1 >= 90 THEN '90일 이상'
-      WHEN DATEDIFF(end_date, start_date) + 1 >= 30 THEN '30일 이상'
-      WHEN DATEDIFF(end_date, start_date) + 1 >= 7 THEN '7일 이상'
-      ELSE 'NONE' END AS duration_type
-FROM car_rental_company_rental_history AS his
-INNER JOIN car_rental_company_car AS car ON car.car_id = his.car_id
-WHERE car.car_type = '트럭')   
+WITH value as (SELECT 
+    history_id,
+    car_type,
+    daily_fee,
+    DATEDIFF(END_DATE,START_DATE) + 1 as date_diff
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY as H
+JOIN CAR_RENTAL_COMPANY_CAR as C
+ON H.CAR_ID = C.CAR_ID
+WHERE C.CAR_TYPE = '트럭')
 
-
-
-SELECT value.history_id, 
-    ROUND(value.daily_fee * value.period * 
-          (100 - IFNULL(plan.discount_rate,0)) / 100) AS FEE
+SELECT 
+*
 FROM value
-LEFT JOIN car_rental_company_discount_plan AS plan 
-    ON plan.duration_type = value.duration_type 
-    AND plan.car_type = value.car_type
-ORDER BY 2 DESC, 1 DESC
+JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN P 
+WHERE P.CAR_TYPE = '트럭'
+
+
+
+  # CASE 
+    #     WHEN DATEDIFF(END_DATE,START_DATE) + 1 >= 90 THEN '90일 이상'
+    #     WHEN DATEDIFF(END_DATE,START_DATE) + 1 >= 30 THEN '30일 이상'
+    #     WHEN DATEDIFF(END_DATE,START_DATE) + 1 >= 7 THEN '7일 이상'
+    # ELSE '7일 미만' END AS DURATION_TYPE
+
