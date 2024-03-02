71eacb8202b1c759a24dd41976e6961f6740ec51
Sangwon Park <wkqkel@naver.com>
2024-03-02 20:29:33 +0900

    [Gold IV] Title: 트리, Time: 96 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.cc
+++ 백준/Gold/4803. 트리/트리.cc
@@ -1,18 +1,15 @@
 #include <iostream>
-#include <vector>
-#include <map>
 
 using namespace std;
 
 const int MX = 502;
-vector<int> vec[MX];
+
 int n, m;
 int a, b;
 int t = 0;
 
-map<int, int> mp;
-
 int par[MX];
+int sz[MX];
 
 int find(int x){
     if(par[x] == x) return x;
@@ -25,34 +22,35 @@ void union_(int a, int b){
     
     if(a==b) return;
     
-    if(b < a) par[a] = b;
-    else par[b] =a;
+    sz[b] += sz[a];
+    par[a] = b;
 }
 
 
 void solution(){
-    for(int i = 1; i <= n; i++) par[i] = i;
+    for(int i = 1; i <= n; i++) {
+        par[i] = i;
+        sz[i] = 1;
+    }
     
     for(int i = 0; i < m; i++){
         cin >> a >> b;
-        int pa = find(a);
-        int pb = find(b);
-        if(pa == pb) {
-            par[pa] = 0;
-            par[pb] = 0;
-            continue;
+        a = find(a);
+        b = find(b);
+        if(a == b) {
+          sz[a] = -1e5;
+          sz[b] = -1e5;
         }
         union_(a,b);
     }
     
     int cnt = 0;
-    bool ch[MX];
-    fill(ch, ch+MX, false);
+    
     for(int i = 1; i <= n; i++){
-        int p = find(i);
-        if(p == 0 || ch[p]) continue;
+        if(sz[find(i)] <= 0) continue;
+        
+        sz[find(i)] = 0;
         cnt++;
-        ch[p] = true;
     }
     
     if(cnt == 0) cout << "No trees.";
@@ -67,7 +65,7 @@ int main()
     while(true){
         t++;
         cin >> n >> m;
-        mp = {};
+ 
         if(n == 0 && m == 0) break;
         cout << "Case " << t << ": ";
         solution();
