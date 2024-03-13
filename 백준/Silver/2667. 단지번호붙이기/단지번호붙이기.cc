20bd59e145e77cde03ea3b4c5e9487379a8d4b96
Sangwon Park <wkqkel@naver.com>
2024-03-13 21:47:37 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,82 +1,67 @@
+/**
+1. 아이디어
+연결요소의 갯수와 각 연결요소의 크기를 오름차순
+dfs
+
+2. 시간복잡도
+dfs: O(V+E) = 25*25 * 5 <= 5e3
+모든요소 순회 = 25*25 <= 1e3
+
+가능
+
+3. 자료구조
+bool visited[MX][MX]
+String board[MX]
+vector<int> vec: 연결요소크기를 담을 곳(정렬필요)
+*/
 #include <iostream>
-#include <queue>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-/**
-map에서 인접한 것들의 집합을 오름차순 정렬해서 찾는 문제.
-
-bfs를 이용
-모든 노드를 탐색하며 갯수 세기
-
-시간복잡도 O(V+E)
-25*25 + 25 *25 *4 => 통과
-*/
-
-const int MX = 25;
-int n;
-string board[MX];
+const int MX = 30;
 bool visited[MX][MX];
-
+string board[MX];
+vector<int> vec;
+int n;
+int cnt = 0;
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
 
-vector<int> vec;
-
-int bfs(pair<int,int> st){
-    queue<pair<int,int>> q;
+void dfs(int x, int y){
+  visited[x][y] = 1;
+  cnt++;
+  
+  for(int i = 0; i < 4; i++){
+    int nx = x + dx[i];
+    int ny = y + dy[i];
     
-    q.push(st);
-    visited[st.first][st.second] = true;
-    
-    int cnt = 0;
-    
-    while(!q.empty()){
-        int sz = q.size();
-        
-        while(sz--){
-            int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
-            
-            for(int i = 0; i < 4; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                
-                if(nx < 0 || nx >= n || ny < 0 || ny >=n) continue;
-                if(visited[nx][ny] || board[nx][ny] =='0') continue;
-                
-                q.push({nx,ny});
-                visited[nx][ny] = true;
-            }
-            cnt++;
-        }
-    }
-    
-    return cnt;
+    if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+    if(visited[nx][ny] || board[nx][ny] == '0') continue;
+    dfs(nx,ny);
+  }
 }
 
-int main()
-{
-    cin >> n;
-    
-    for(int i = 0; i < MX; i++){
-        cin >> board[i];
+int main(){
+  cin >> n;
+  for(int i = 0; i < n; i++){
+    cin >> board[i];
+  }
+  
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j <n; j++){
+      if(board[i][j]=='0' || visited[i][j]) continue;
+      cnt = 0;
+      dfs(i,j);
+      vec.push_back(cnt);
     }
-    
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            if(board[i][j] == '0' || visited[i][j]) continue;
-            vec.push_back(bfs({i,j}));
-        }
-    }
-    
-    sort(vec.begin(), vec.end());
-    
-    cout << vec.size() << '\n';
-    for(int i = 0; i < vec.size(); i++){
-        cout << vec[i] << '\n';
-    }
+  }
+  
+  sort(vec.begin(), vec.end());
+  
+  cout << vec.size() << '\n';
+  for(int i =0 ; i < vec.size(); i++){
+    cout << vec[i] << "\n";
+  }
 }
