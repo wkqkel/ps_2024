cc95e9a4f4a642a919f5022d4e296372905055e0
Sangwon Park <wkqkel@naver.com>
2024-03-21 12:17:12 +0900

    [level 2] Title: 롤케이크 자르기, Time: 4.47 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -1,36 +1,21 @@
 #include <string>
 #include <vector>
-#include <map>
+#include <set>
 #include <iostream>
 using namespace std;
-
-const int MX = 1e6+2;
-int arr1[MX];
-int sz1 = 0;
-
-int arr2[MX];
-int sz2 = 0;
 
 int solution(vector<int> arr) {
     int n = arr.size();
     int cnt = 0;
     
-    for(int i = 0; i < n; i++){
-        int cur = arr[i];
-        if(arr2[cur] == 0) sz2++;
-        arr2[cur]++;
-    }
-    
-    int res = 0;
-    
-    for(int i = 0; i < n; i++){
-        int cur = arr[i];
-        if(arr1[cur] ==0) sz1++;
-        arr1[cur]++;
-        arr2[cur]--;
-        if(arr2[cur] ==0) sz2--;
-        if(sz1 == sz2) res++;
-    }
-
-    return res;
+//     for(int i = 0; i < n; i++){
+//         set<int> s1;
+//         set<int> s2;
+//         s1.remove()
+//         if(s1.size() == s2.size()) cnt++;
+//     }
+    set<int> s1;
+    s1.insert(3);
+    s1.get(3);
+    return cnt;
 }
