c578e0bbce19f6f736d9358b3bb48670adef94d0
Sangwon Park <wkqkel@naver.com>
2024-04-02 23:25:13 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+queue<pair<int,int>> q;
+
+int solution(int bridge, int weight, vector<int> trucks) {
+    int idx = 0;
+    int cur = 0;
+    int sum = 0;
+    
+    while(true){
+        // 탈출조건
+        
+        if(trucks.size() <= idx && q.empty()) break;
+        cur++;
+        // 큐가 비지않았으면,
+        while(!q.empty()){
+            int w = q.front().first;
+            int prev = q.front().second;
+            if(cur - prev < bridge) break;
+            // cout << cur << " " << w << " " << q.size()<< '\n';
+            q.pop();
+            sum -= w;
+        }
+        
+        if(sum + trucks[idx] > weight ||trucks.size() <= idx  ) continue; 
+    
+        // cout << cur << " " << trucks[idx] << "\n";
+        q.push({trucks[idx], cur});
+        sum += trucks[idx];
+        idx++;
+    }
+              
+    return cur;
+}
