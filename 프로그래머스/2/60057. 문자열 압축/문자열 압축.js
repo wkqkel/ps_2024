f7bb6dbb982dee2aa609b82b5c39d9d085475265
Sangwon Park <wkqkel@naver.com>
2024-04-19 00:10:28 +0900

    [level 2] Title: 문자열 압축, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
@@ -1,28 +1,26 @@
-function solution(s) {
-    const n = s.length;
+function solution(str) {
+    const n = str.length;
     let mn = 1e9;
+    
     for(let i = 1; i <= n; i++){
-        let res = ""
-        let prev = s.slice(0, i);
+        let s = 0;
+        let e = i;
+        let cur, nxt;
         let cnt = 1;
- 
-
-        for(let j = i; j < n; j+= i){
-            const cur = s.slice(j, j+i);
-     
-            if(prev == cur) cnt++;
+        let res = "";
+        while(cur != ''){
+            cur = str.slice(s, e);
+            nxt = str.slice(s+i, e+i);
+            if(cur == nxt) cnt++;
             else {
-                if(cnt != 1) res += cnt
-                res += prev;
-                prev = cur;
-         
+                if(cnt!= 1) res += cnt;
+                res += cur;
                 cnt = 1;
             }
+            s+=i;
+            e+=i;
         }
-        if(cnt != 1) res += cnt
-     
-        res += prev;
-        
+ 
         mn = Math.min(mn, res.length);
     }
     return mn;
