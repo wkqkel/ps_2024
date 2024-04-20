89c369d93751b572d9bcdc53e4098a49c27ab31f
Sangwon Park <wkqkel@naver.com>
2024-04-21 03:27:27 +0900

    [level 2] Title: 후보키, Time: 1.00 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.js
+++ 프로그래머스/2/42890. 후보키/후보키.js
@@ -2,17 +2,9 @@ let arr = [];
 const ch = {};
 let res = 0;
 
-
-function has(a, b){
-    return [...a].every(v=> b.includes(v))
-}
-
-
-
 function recur(cur, st, n, r){
     if(cur == n) {
         const cur = arr.join("");
-         // console.log({arr,n})
         if(Object.keys(ch).some(k=> has(k, cur))) return;
        
         const d = r.map(v=>v.filter((_,i)=> arr.includes(i)).join(""))
@@ -30,17 +22,22 @@ function recur(cur, st, n, r){
         recur(cur + 1, i+1 , n, r);
     }
 }
-// 0이 됐으면 01은안돼
-// 12는되고, 13도 됨. 123은 안됨.
-// 145가 됨. 1245가 있으면 안됨.
+
 function solution(r) {
-    // console.log(has("145","1245"), true)
-    // console.log(has("12", "13"), false)
-    // console.log(has("12", "123"), true)
-    // console.log(has("0", "01"),true)
     for(let i = 1; i <= r[0].length; i++){
         recur(0, 0, i, r);
     }
     
     return res;
+}
+
+// 0이 됐으면 01은안돼
+// 12는되고, 13도 됨. 123은 안됨.
+// 145가 됨. 1245가 있으면 안됨.
+// console.log(has("145","1245"), true)
+// console.log(has("12", "13"), false)
+// console.log(has("12", "123"), true)
+// console.log(has("0", "01"),true)
+function has(a, b){
+    return [...a].every(v=> b.includes(v))
 }
