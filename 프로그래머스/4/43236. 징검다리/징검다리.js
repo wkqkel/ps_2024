30b7fe7b13f5b766163e7f27344424f265d3db57
Sangwon Park <wkqkel@naver.com>
2024-04-20 15:59:49 +0900

    [level 4] Title: 징검다리, Time: 24.10 ms, Memory: 40 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.js
+++ 프로그래머스/4/43236. 징검다리/징검다리.js
@@ -2,6 +2,8 @@
 특정값이 최소일 때 제거해야하는 바위의 갯수를 찾고
 해당 갯수가 n보다 크면 최솟값을 줄이고,
 같거나 작으면 늘려야함.
+
+못풀어서 https://mjmjmj98.tistory.com/78 참고
 */
 function solution(distance, rocks, n) {
     const arr = [0, ...rocks.sort((a,b)=>a-b), distance];
