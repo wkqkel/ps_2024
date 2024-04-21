79201e52f0446d47b0ae3e9e0ca39abd79a54cc9
Sangwon Park <wkqkel@naver.com>
2024-04-21 22:56:15 +0900

    [level 2] Title: 테이블 해시 함수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.js
@@ -0,0 +1,14 @@
+function sort(a,b){
+  
+}
+
+function solution(data, col, row_begin, row_end) {
+   return data
+       .sort((a,b)=> {
+        if(a[col-1] - b[col-1]!=0) return a[col-1] - b[col-1];
+        else return  b[0] - a[0];
+        })
+       .map((v,i)=> v.reduce((acc,cur)=> acc + cur % (i+1),0))
+       .filter((v,i)=> row_begin <= i+1 && i + 1 <= row_end)
+       .reduce((acc,cur)=> cur^acc);
+}
