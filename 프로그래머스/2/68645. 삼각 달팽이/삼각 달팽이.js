3426e8bf0111852734c60ed43af683505429498c
Sangwon Park <wkqkel@naver.com>
2024-04-25 23:46:10 +0900

    [level 2] Title: 삼각 달팽이, Time: 112.55 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -4,15 +4,14 @@ function solution(n) {
   const arr = Array.from({length: n}, (v,i)=> new Array(i+1).fill(null));
   let dir = 0;
   let cur = 1;
-  let x = 0, y = 0;
+  let x = -1, y = 0;
   const last = Math.floor((1 + n) * n / 2);  
-  arr[0][0] = cur++;
-    
+ 
   while(cur <= last){
       const nx = x + DIRS[dir][0];
       const ny = y + DIRS[dir][1];
 
-      if((nx < 0 || nx >= n || ny < 0 || ny >= n) || arr[nx][ny])  {
+      if(nx >= n || ny >= n || arr[nx][ny])  {
          dir = (dir+1) % 4;
       } else {
          arr[nx][ny] = cur++;
