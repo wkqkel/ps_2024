30f76cf421302fe4d93fe111705f24b5617238e8
Sangwon Park <wkqkel@naver.com>
2024-04-25 13:55:13 +0900

    [level 2] Title: 순위 검색, Time: 649.36 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.js
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.js
@@ -3,7 +3,6 @@
 
 경우의 수 2^6에 대해 배열 정렬해두고, => 108
 거기서 이진탐색으로 upperBound 점수만 찾아서 카운트
-
 */
 function getLowerBound(arr, find){
     let s = 0;
@@ -31,11 +30,10 @@ function solution(infos, query) {
         const [언어, 직군,경력,음식,점수] = info.split(" ");
         const keys = [언어, 직군,경력,음식];
         const tmp = [];
-        
         function recur(cur){
             if(cur == 4){
                 const key = tmp.join("")
-                if (Array.isArray(map[key])) map[key].push(점수);
+                if(map[key]) map[key].push(점수);
                 else map[key] = [점수];
                 return;
             }
@@ -50,10 +48,12 @@ function solution(infos, query) {
         map[k] = map[k].map(Number).sort((a,b)=> a-b);
     }
 
+ 
     const res = [];
     for(const q of query){
-        const [언어, 직군,경력,음식,점수] = q.replaceAll(" and", "").split(" ");
+        const [언어, 직군,경력,음식,점수] = q.replace(/ and/g, '').split(" ");
         const key = [언어,직군,경력,음식].join("");
+
         const arr = map[key];
         if(!arr) {
             res.push(0);
