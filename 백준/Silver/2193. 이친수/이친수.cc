458928014b4b07cab94129802bda24ce41537138
Sangwon Park <wkqkel@naver.com>
2024-05-23 16:16:32 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.cc
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -4,15 +4,18 @@ using namespace std;
 
 long dp[100];
 
-int main()
-{
+int main(){
     int n;
     cin >> n;
+    
+ 
     dp[1] = 1;
-    dp[2] = 1;
-    for(int i = 3; i <= n; i++){
-        dp[i] = dp[i-1] + dp[i-2];
+    for(int i = 2; i <= n; i++){
+        dp[i] = dp[i-2] + dp[i-1];
     }
+    
     cout << dp[n];
+   
+
     return 0;
 }
