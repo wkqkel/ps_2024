d0ab24ef5b8905b1ac04d60fa8badf01452e0219
Sangwon Park <wkqkel@naver.com>
2024-05-23 15:57:52 +0900

    [Gold V] Title: 동전 1, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.cc
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -2,24 +2,27 @@
 
 using namespace std;
 
-int n, k;
-int coin[102];
-long dp[10020];
+const int MX = 100002;
+int coins[102];
+int dp[MX];
 
-int main()
-{
+int main(){
+    int n, k;
     cin >> n >> k;
     
-    for(int i = 0; i < n; i++) cin >> coin[i];
-    
+    for(int i = 0; i < n; i++){
+        cin >> coins[i];
+    }
     dp[0] = 1;
     for(int i = 0; i < n; i++){
-        for(int j = coin[i]; j <= k; j++){
-            dp[j] = dp[j] + dp[j-coin[i]];
-        } 
+        int coin = coins[i];
+        for(int j = 1; j <= 10000; j++){
+            if(j-coin <0) continue;
+            dp[j] += dp[j-coin];
+        }
     }
-      
-    cout << dp[k];
     
+    cout << dp[k];
+
     return 0;
 }
