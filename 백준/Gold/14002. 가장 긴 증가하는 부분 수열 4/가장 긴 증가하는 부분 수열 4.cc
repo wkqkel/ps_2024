b0e71534607bb337c28937d45463ac6459f9b0b9
Sangwon Park <wkqkel@naver.com>
2024-05-23 17:31:41 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -1,68 +1,50 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int n;
-int A[1002];
 
+vector<pair<int, int>> vec;
 int dp[1002];
-
-/**
-LIS 문제 + 경로추적
-LIS
-dp[x]를 A[x]를 마지막 값으로 가질 때 가장 긴 부분 수열의 길이로 정의
-
-이중포문을 돌면서,
-초기 mx를 0으로 두고,
-자신보다 앞에 있는 값 중 더 작은 값을 가진 dp들의 최댓값 + 1로 설정.
-
-경로추적은
-이렇게 구한 최댓값에서,
-앞에 있는 값중 길이가 -1씩이면서 더 작은 값을 찾으면 됨.
-
-*/
+int arr[1002];
 
 int main()
 {
+    int n;
     cin >> n;
-    for(int i = 0; i < n; i++){
-        cin >> A[i];
-    }
+    for(int i = 0; i < n; i++) cin >> arr[i];
     
     for(int i = 0; i < n; i++){
         int mx = 0;
         for(int j = 0; j < i; j++){
-            if(A[i] > A[j]) mx = max(mx, dp[j]);
+            if(arr[j] >= arr[i]) continue;
+            mx = max(dp[j], mx);
         }
-        dp[i] = mx+1;
+        dp[i] = max(dp[i], mx + 1);
     }
     
-    int mx = -1;
-    int mxIdx = -1;
+    int mx = 0;
+    int idx = -1;
     for(int i = 0; i < n; i++){
-        if(mx < dp[i]){
+        if(dp[i] > mx){
             mx = dp[i];
-            mxIdx = i;
+            idx = i;
         }
     }
- 
-    vector<int> vec;
-    int cur = mx-1;
-    vec.push_back(A[mxIdx]);
-    
-    for(int i = mxIdx-1; i >= 0; i--){
-        if(dp[i] == cur && A[i] < vec[vec.size()-1]){
-            vec.push_back(A[i]);
+
+    int cur = mx;
+    vector<int> path;
+    for(int i = idx; i >= 0; i--){
+        if(cur == dp[i]){
+            path.push_back(arr[i]);
             cur--;
         }
     }
     
-    cout << mx << "\n";
-    
-    for(int i = vec.size()-1; i >= 0; i--){
-        cout << vec[i] << " ";
-    }
+    reverse(path.begin(), path.end());
+    cout << path.size() << "\n";
+    for(int v: path) cout << v << " ";
     
     return 0;
 }
