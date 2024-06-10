33139260993b1899ecf1ff70f934a1c7f9e5057d
Sangwon Park <wkqkel@naver.com>
2024-06-10 15:52:16 +0900

    [Gold V] Title: 토마토, Time: 252 ms, Memory: 8660 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.cc
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -4,19 +4,20 @@
 using namespace std;
 
 const int MX = 102;
-
-int dz[6] = {1,-1,0,0,0,0};
-int dx[6] = {0,0,1,0,-1,0};
-int dy[6] = {0,0,0,1,0,-1};
+int board[MX][MX][MX];
+bool ch[MX][MX][MX];
 
 int n,m,h;
-int board[MX][MX][MX];
-queue<pair<int,pair<int,int>>> q;
-int bfs(){
-    int d = 0;
+
+queue<pair<int, pair<int, int>>> q;
+int dz[6] = {0,0,0,0,1,-1};
+int dx[6] = {1,0,-1,0,0,0};
+int dy[6] = {0,1,0,-1,0,0};
+int days = 0;
+
+void bfs(){
     while(!q.empty()){
         int sz = q.size();
-   
         while(sz--){
             int z = q.front().first;
             int x = q.front().second.first;
@@ -28,47 +29,48 @@ int bfs(){
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 
-                if(nx < 0 || nx >= n || ny <0 || ny >= m || nz <0 || nz >= h) continue;
-                if(board[nz][nx][ny] != 0) continue;
+                if(nz < 0 || nz >= h || nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if(ch[nz][nx][ny] || board[nz][nx][ny] != 0) continue;
                 q.push({nz,{nx,ny}});
-                board[nz][nx][ny] = 1;
+                ch[nz][nx][ny] = 1;
             }
         }
-        d++;
+        days++;
     }
-    
-    return d;
 }
 
 int main()
 {
     cin >> m >> n >> h;
     
-    for(int i = 0; i < h; i++){
-        for(int j = 0; j < n; j++){
-            for(int k = 0; k < m; k++){
-                cin >> board[i][j][k];
-                if(board[i][j][k] == 1) {
-                    q.push({i,{j,k}});
+    int unused = 0; 
+    for(int k = 0; k < h; k++){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                cin >> board[k][i][j];
+                if(board[k][i][j] == 1) {
+                    q.push({k,{i,j}});
+                    ch[k][i][j] = 1;
+                } else if(board[k][i][j] == 0){
+                    unused++;
                 }
+            }
+        }
+    }
+    bfs();
+
+    int cnt = 0;
+    for(int k = 0; k < h; k++){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(ch[k][i][j] && board[k][i][j] == 0) cnt++;
             }
         }
     }
     
-    int cnt = bfs();
-    int flag = true;
-    for(int i = 0; i < h; i++){
-        for(int j = 0; j < n; j++){
-            for(int k = 0; k < m; k++){
-                if(board[i][j][k] == 0) {
-                   flag = false;
-                }
-            }
-        }
-    }
-
-    if(flag) cout << cnt-1;
-    else cout << -1;
+    if(unused == 0) cout << 0;
+    else if(unused != cnt) cout << -1;
+    else cout << days - 1;
     
     return 0;
 }
