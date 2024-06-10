fb83868f7a3ce3b67b110a301641c9feb1800c91
Sangwon Park <wkqkel@naver.com>
2024-06-10 15:21:48 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2068 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -1,73 +1,90 @@
 #include <iostream>
+#include <map>
+#include <queue>
 
 using namespace std;
 
-/**
-1. 아이디어
-- dfs로 연결요소의 갯수 세기
-- 적록색약을 위한 board2 색 하나 통일
-
-
-2. 시간복잡도
-dfs: O(V+E) = 5e4;
-*/
-
 int n;
 const int MX = 102;
-char board1[MX][MX];
-char board2[MX][MX];
 
-bool visited[MX][MX];
+char board1[MX][MX];
+bool ch1[MX][MX];
+int cnts1[3];
+
+char board2[MX][MX];
+bool ch2[MX][MX];
+int cnts2[3];
+
+map<char, int> color_map = {{'R', 0}, {'G',1}, {'B',2}};
+
+
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
 
-void dfs(int x, int y, char v, char board[MX][MX]){
-    visited[x][y] = 1;
+void bfs(int sx, int sy, auto& board, auto& ch){
+    queue<pair<int,int>> q;
     
-    for(int i = 0; i < 4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
+    q.push({sx,sy});
+    ch[sx][sy] = 1;
+    
+    while(!q.empty()){
+        int sz = q.size();
+        while(sz--){
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
         
-        if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-        if(visited[nx][ny] || board[nx][ny] != v) continue;
-        dfs(nx,ny, v, board);
+            for(int i = 0; i < 4; i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                
+                if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+                if(ch[nx][ny] || board[x][y] != board[nx][ny]) continue;
+                q.push({nx,ny});
+                ch[nx][ny] = 1;
+            } 
+        }
+     
     }
 }
 
 int main()
 {
     cin >> n;
+    
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            cin >> board1[i][j];
-            board2[i][j] = board1[i][j];
-            if(board2[i][j] == 'R') board2[i][j] ='G';
+            char c;
+            cin >> c;
+            board1[i][j] = c;
+            if(c == 'G'){
+                board2[i][j] = 'R';
+            } else {
+                board2[i][j] = c;
+            }
         }
     }
-    int cnt1 = 0;
+
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            if(visited[i][j]) continue;
-            dfs(i,j, board1[i][j], board1);
-            cnt1++;
+            if(ch1[i][j]) continue;
+            bfs(i,j, board1, ch1);
+            cnts1[color_map[board1[i][j]]]++;
         }
     }
     
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            visited[i][j] = 0;
+            if(ch2[i][j]) continue;
+            bfs(i,j, board2, ch2);
+            cnts2[color_map[board2[i][j]]]++;
         }
     }
     
-    int cnt2 = 0;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            if(visited[i][j]) continue;
-            dfs(i,j, board2[i][j], board2);
-            cnt2++;
-        }
-    }
     
-    cout << cnt1 << " " << cnt2;
+    int sum1 = cnts1[0] + cnts1[1] + cnts1[2];
+    int sum2 = cnts2[0] + cnts2[1] + cnts2[2];
+    
+    cout << sum1 << " " << sum2;
     return 0;
 }
