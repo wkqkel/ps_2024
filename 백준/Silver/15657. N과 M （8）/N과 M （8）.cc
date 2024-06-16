6dc82b349d0e23b710d11c5b9d65c9f7a56bd575
Sangwon Park <wkqkel@naver.com>
2024-06-16 16:04:42 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int arr[20];
+
+int val[20];
+
+void recur(int cur, int s){
+    if(cur == m){
+        for(int i = 0; i < m; i++) cout << val[arr[i]] << " ";
+        cout << "\n";
+        return;
+    }
+    for(int i = s; i < n; i++){
+        arr[cur] = i;
+        recur(cur + 1, i);
+    }
+}
+int main()
+{
+    cin >> n >> m;
+    
+    for(int i = 0; i < n; i++){
+        cin >> val[i];
+    }
+    sort(val, val+n);
+    
+    recur(0, 0);
+
+    return 0;
+}
