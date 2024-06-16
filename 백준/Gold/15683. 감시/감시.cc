fc7ae57f852c9fac90aa0b415cc41acb4a2e38d4
Sangwon Park <wkqkel@naver.com>
2024-06-16 18:42:12 +0900

    [Gold III] Title: 감시, Time: 32 ms, Memory: 5108 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.cc
+++ 백준/Gold/15683. 감시/감시.cc
@@ -1,106 +1,142 @@
-#include <bits/stdc++.h>
+    #include <iostream>
+#include <vector>
+
 using namespace std;
 
+/**
+접근
+- 사각지대의 최소갯수구하기
+-CCTV마다 방향이 있고, 벽을 통과할 수 없음.
+
+1. cctv 방향 경우의 수
+4(방향)^8(최대CCTV) = 2^16 = 2^10 * 2^6 = 1024 * 64 = 약 60000
+
+2. board업데이트
+cctv마다 벽을 만날때까지 WATCHED로 변경
+(CCTV는 건너뜀, 맵을 벗어나면 끝냄)
+4(방향) * 8(최대맵) * 8(최대CCTV) = 256
+
+3. 사각지대갯수 카운트
+아직 0인것 세기
+8*8
+
+총 시간복잡도 60000 * (256 + 64) 정도로 가능
+*/
+
 int n, m;
-int board1[10][10];
-int board2[10][10];
-int dx[4] = {0, 1, 0, -1};
-int dy[4] = {1, 0, -1, 0};
+const int MX = 10;
+int board[MX][MX];
+int board2[MX][MX];
 
-bool oop(int x, int y){
-  return 0 > x || x >= n || 0 > y || y >= m;
+const int WALL = 6;
+const int WATCHED = 7;
+
+int tmp[MX];
+vector<pair<int,int>> cctvs;
+vector<string> cases;
+
+
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, -1, 0, 1};
+
+void update(int x, int y, int dir){
+    dir %= 4;
+    int nx = x;
+    int ny = y;
+    while(true){
+        nx += dx[dir];
+        ny += dy[dir];
+        if(nx < 0 || nx >= n || ny < 0 || ny >= m) break;
+        if(board2[nx][ny] == WALL) break;
+        if(board2[nx][ny] != 0) continue;
+        board2[nx][ny] = WATCHED;
+    }
 }
 
-void upd(int x, int y, int dir){
-  dir %= 4;
-
-  while(true) {
-    x += dx[dir];
-    y += dy[dir];
-    if(oop(x,y) || board1[x][y] == 6) break;
-    if(board1[x][y] != 0) continue; 
-    board2[x][y] = 7;
-  }
+void monitor(int x, int y, int dir){
+    int cctv = board2[x][y];
+  
+    if(cctv == 1){
+        update(x, y, dir);
+    } 
+    else if(cctv == 2){
+        update(x, y, dir);
+        update(x, y, dir+2);
+    }
+    else if(cctv == 3){
+        update(x, y, dir);
+        update(x, y, dir+3);
+    }
+    else if(cctv == 4){
+        update(x, y, dir);
+        update(x, y, dir+2);
+        update(x, y, dir+3);
+    }
+    else if(cctv == 5){
+        update(x, y, dir);
+        update(x, y, dir+1);
+        update(x, y, dir+2);
+        update(x, y, dir+3);
+    }
 }
 
-
-
-
-int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-
-  cin >> n >> m;
-
-  int mn = 0;
-
-  vector<pair<int,int>> cctv;
-
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j < m; j++){
-      cin >> board1[i][j];
-      if(board1[i][j] != 0 && board1[i][j] != 6) {
-        cctv.push_back({i,j});
-      }
-      if(board1[i][j] == 0) mn++;
+void recur(int cur){
+    if(cur == cctvs.size()){
+        string str = "";
+        for(int i = 0; i < cctvs.size(); i++) {
+            str += to_string(tmp[i]);
+        }
+        cases.push_back(str);
+        return;
     }
-  }
-
-  for(int j = 0; j < n; j++){
-    for(int k = 0; k < m; k++){
-      board2[j][k] = board1[j][k];
+    for(int i = 0; i < 4; i++){
+        tmp[cur] = i;
+        recur(cur+1);
     }
-  }
+}
 
-
-
-  for(int i = 0; i < 1 << (2 * cctv.size()); i++ ){
-    // board2 원복 복사
-    for(int j = 0; j < n; j++){
-      for(int k = 0; k < m; k++){
-        board2[j][k] = board1[j][k];
-      }
+int main()
+{
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> board[i][j];
+            if(0 < board[i][j] && board[i][j] < 6) {
+                cctvs.push_back({i,j});
+            }
+        }
     }
-    // 방향 경우의 수 하나에 cctv만큼 돌고, 해당 방향 처리
-    int tmp = i;
-
-    for(int j = 0; j < cctv.size(); j++){
-      int dir = tmp % 4;
-      tmp /= 4;
-
-      int x = cctv[j].first;
-      int y = cctv[j].second;
-      int cctv_type = board1[x][y];
-
-      if(cctv_type == 1){
-        upd(x,y,dir);
-      } 
-      else if (cctv_type == 2){
-        upd(x,y,dir);
-        upd(x,y,dir+2);
-      }
-      else if (cctv_type == 3){
-        upd(x,y,dir);
-        upd(x,y,dir+1);
-      }
-      else if (cctv_type == 4){
-        upd(x,y,dir);
-        upd(x,y,dir+1);
-        upd(x,y,dir+2);
-      } else {
-        upd(x,y,dir);
-        upd(x,y,dir+1);
-        upd(x,y,dir+2);
-        upd(x,y,dir+3);
-      }
+    recur(0);
+    int mn = 1e9;
+    // 1. cctv방향 경우의 수에 대해
+    for(string cc : cases){
+        int cnt = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                board2[i][j] = board[i][j];
+            }
+        }
+        
+        // 2. cctv를 돌면서 board update
+        for(int i = 0; i < cctvs.size(); i++){
+            int x = cctvs[i].first;
+            int y = cctvs[i].second;
+            int dir = (int) (cc[i] - '0');
+          
+            monitor(x, y, dir);
+        }
+  
+        // 3. 사각지대 카운트
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(board2[i][j] == 0) cnt++;
+            }
+        }
+        
+        mn = min(cnt, mn);
     }
-    int cnt =0;
-    for(int j = 0; j < n; j++){
-      for(int k = 0; k < m; k++){
-        if(board2[j][k] == 0) cnt++; 
-      }
-    }
-    mn = min(mn, cnt);
-  }
-  cout << mn;
+
+    cout << mn;
+
+    return 0;
 }
