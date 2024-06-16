39766542490b9e65a37610cf380b9ad1315e343a
Sangwon Park <wkqkel@naver.com>
2024-06-16 15:38:07 +0900

    [Silver III] Title: N과 M (1), Time: 36 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -2,29 +2,29 @@
 
 using namespace std;
 
+int n, m;
+
 int arr[10];
 bool ch[10];
-int n,m;
-
-void recur(int cur){
-    if(cur == m) {
-        for(int i = 0; i < m; i++) cout << arr[i] << ' ';
-        cout << '\n';
-        return;
-    }
     
+void recur(int cur){
+    if(cur == m){
+        for(int i = 0; i < m; i++) cout << arr[i] << " ";
+        cout << '\n';
+    }
     for(int i = 1; i <= n; i++){
         if(ch[i]) continue;
+        ch[i] = 1;
         arr[cur] = i;
-        ch[i] = true;
         recur(cur+1);
-        ch[i] = false;
+        ch[i] = 0;
+        arr[cur] = 0;
     }
 }
 int main()
 {
     cin >> n >> m;
+    
     recur(0);
-
     return 0;
 }
