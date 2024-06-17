87a4ef667e32bbdd0c5842ea7e9714cff06ddc7e
Sangwon Park <wkqkel@naver.com>
2024-06-17 16:15:02 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,47 +1,101 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-#define X first
-#define Y second
+/**
+# Boj 15686 치킨거리
+## 문제이해
+- N*N도시
+- 빈칸,집,가게 = {0,1,2}
+- 치킨거리: 현재 집에서 가장 가까운 가게 거리
+- 도시치킨거리: 각집의 치킨거리의 합
+- M은 살아남을 치킨집 개수
+
+## 설계
+1. 치킨집 고르는 경우의 수 구하기
+- 치킨집 최대 13개 중에서 M개 고르기
+- 최대 13C6 
+
+2. 각 집의 치킨거리 구하기
+- 살아남은 치킨 -> 집 이중 돌면서 최소 치킨거리 구하기
+- 이 때 집마다 돌면서, 최소 치킨거리 갱신
+- 13 * 13
+3. 도시치킨거리 최소합 갱신
+- 집마다 돌면서 sum 구하기
+- sum의 min값 구하기
+
+---
+집의 개수는 2N을 넘지않음 => dist의 MX 102 설정;
+치킨집과 M의 최대 개수는 13 => arr의 MX 20설정
+*/
+
 
 int n, m;
-int board[52][52];
-int mn = 1e7;
 
-vector<pair<int,int>> house;
-vector<pair<int,int>> chicken;
-    
-int main(void)
+vector<pair<int, int>> stores;
+vector<pair<int, int>> houses;
+int dists[102];
+vector<vector<int>> tests;
+vector<int> arr;
+
+void recur(int cur, int st){
+    if(cur == m){
+        tests.push_back(arr);
+        return;
+    }
+    for(int i = st; i < stores.size(); i++){
+        arr.push_back(i);
+        recur(cur+1, i+1);
+        arr.pop_back();
+    }
+}
+
+int get_dist(auto& x, auto& y){
+    return abs(x.first - y.first) + abs(x.second - y.second);
+}
+
+int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
     cin >> n >> m;
-
-    for(int i = 0; i < n; i++) {
+    
+    for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            cin >> board[i][j];
-            if(board[i][j] == 1) house.push_back({i,j});
-            if(board[i][j] == 2) chicken.push_back({i,j}); 
+            int v;
+            cin >> v;
+            if(v == 2) stores.push_back({i,j});
+            else if(v == 1) houses.push_back({i,j});
         }
     }
     
-    vector<int> brute(chicken.size(), 1);
-    fill(brute.begin(), brute.begin() + chicken.size() - m, 0);
-    
-    do{
-        int dist = 0;
-        for(auto h : house){
-            int tmp = 1e7;
-            for(int i = 0; i < chicken.size(); i++){
-                if(brute[i] == 0) continue;
-                tmp = min(tmp, abs(h.X - chicken[i].X)+abs(h.Y - chicken[i].Y));
-            }
-            dist += tmp;
+    recur(0, 0);
+
+    int mn = 1e9;
+    // 1. 살아남은 가게 경우의수
+    for(auto t : tests){
+        for(int i = 0; i < 102; i++){
+            dists[i] = 1e9;
         }
-        mn = min(mn, dist);
-    }while(next_permutation(brute.begin(),brute.end()));
+        // 2-1. 먼저 살아남은 치킨집 돌고
+        for(int alive_idx: t){
+            auto s = stores[alive_idx];
+             // 2-2. 집 돌면서 최솟값 갱신
+            for(int i = 0; i < houses.size(); i++){
+                auto h = houses[i];
+                dists[i] = min(dists[i], get_dist(h, s));
+            }
+        }
+        // 3. 도시거리 구해서, 최솟값 갱신
+        int sum = 0;
+        for(int i = 0; i < houses.size();i++){
+            sum += dists[i];
+        }
+        
+        mn = min(sum, mn);
+    }
     
     cout << mn;
+
+    return 0;
 }
