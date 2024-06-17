f076c0c18755080b382b97e1cbbf18ddf94be11b
Sangwon Park <wkqkel@naver.com>
2024-06-17 15:02:55 +0900

    [Gold II] Title: 2048 (Easy), Time: 44 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
@@ -1,80 +1,139 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 /**
-1. 아이디어
-- 먼저 한방향에 대해서 tilt함수
-- 한줄씩 돌아가며 tilt함수 실행
-- 방향은 실제 맵을 돌려가며 구현
-- 최대 5번까지 이동가능인데, 이동할 수록 적은 수가 나올 확률은 없으므로 5번 이동
+설계
+1. 경우의 수 구하기
+5번 이동시킨 경우의 수
+4^5 = 2 ^ 10
+2. 회전하기
+방향에 따라 판자체를 회전한뒤
+20 * 20 * 4
+3. 이동 & 합치기
+부딪힐 때까지 방향으로 가고,
+같은 수 && 이미 합쳐진게 아니라면, 합치기
+20  * 20 * 20 
 
-2. 시간복잡도
-- (20 * 20) * 5 * 4^5
+4. 보드에서 가장 큰 블록 출력하기 
+20 * 20
 
-3. 자료구조
-- board1: 원본
-- board2: 업데이트된 배열
-- mx: 가장 큰 블록의 값
-*/ 
-#include <bits/stdc++.h>
-using namespace std;
+--- 
+설계시 고려못한점 & 막힌 구간
+1. 전체가 아닌 한번의 이동에서 합쳐진 것만 다시 못합침
++
+2. 매테스트케이스마다 보드 초기화
+*/
 
-int board1[21][21];
-int board2[21][21];
+const int MX = 22;
 int n;
+int origin[MX][MX];
+int board[MX][MX];
+int tmp[MX][MX];
+int arr[MX];
+
+vector<string> cases;
+
+void recur(int cur){
+    if(cur == 5){
+        string str = "";
+        for(int i = 0; i < 5; i++){
+            str += to_string(arr[i]);
+        } 
+        cases.push_back(str);
+        return;
+    }
+    for(int i = 0; i < 4; i++){
+        arr[cur] = i;
+        recur(cur+1);
+    }
+}
 
 void rotate(){
-	int tmp[21][21];
-	for(int i = 0; i< n; i++)
-		for(int j = 0; j < n; j++)
-			tmp[i][j] = board2[i][j];
-	for(int i =0; i<n; i++)
-		for(int j = 0; j < n; j++)
-			board2[i][j] = tmp[n-1-j][i];
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n;j++){
+            tmp[j][n-i-1] = board[i][j];
+        }
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            board[i][j] = tmp[i][j];
+        }
+    }
 }
 
-void tilt(int dir){
-	int back = 3 - dir;
-	while(dir--) rotate();
-	for(int i = 0; i < n; i++) {
-		int tilted[21] = {};
-		int idx = 0;
-		for(int j = 0; j<n;j++) {
-			if(board2[i][j] == 0) continue;
-			if(tilted[idx] == 0) 
-				tilted[idx] = board2[i][j];
-			else if(tilted[idx] == board2[i][j])
-				tilted[idx++] *= 2;
-			else 
-				tilted[++idx] = board2[i][j];
-		}
-		for(int j = 0; j<n; j++) board2[i][j] = tilted[j];
-	}
-	while(back--) rotate();
+void move(auto& arr){
+    bool ch[n];
+    fill(ch, ch+n, false);
+    
+    for(int i = 1; i < n; i++){
+        int cur = i;
+        int pre = i - 1;
+           
+        while(true){
+            if(pre < 0) break;
+            if(arr[pre] == 0) {
+                swap(arr[pre], arr[cur]);
+                swap(ch[pre], ch[cur]);
+            }
+            else if(arr[pre] == arr[cur]){
+                if(ch[pre] || ch[cur]) break;
+                arr[pre] *= 2;
+                arr[cur] = 0;
+                ch[pre] = true;
+                ch[cur] = false;
+            }
+            pre--;
+            cur--;
+        }
+    }
 }
 
-int main(void){
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> n;
-	for(int i = 0; i< n; i++)
-		for(int j = 0; j < n; j++)
-			cin >> board1[i][j];
-	int mx = 0;
-	for(int tmp = 0; tmp < 1024; tmp++){ // 4^5
-		for(int i = 0; i< n; i++)
-			for(int j = 0; j < n; j++)
-				board2[i][j] = board1[i][j];
-		int brute = tmp;
-		for(int i = 0; i < 5; i++) {
-			int dir = brute % 4;
-			brute /= 4;
-			tilt(dir);
-		}
-		for(int i = 0; i < n; i++)
-			for(int j = 0; j < n; j++)
-				mx= max(mx, board2[i][j]);
-	}
-	cout << mx;
+
+int main()
+{
+    cin >> n;
+    for(int i =0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> origin[i][j];
+        }
+    }
+    recur(0);
+    
+  
+    int mx = -1;
+    // 1. 경우의 수 구하기
+    for(string t : cases){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                board[i][j] = origin[i][j];
+            }
+        }
+        
+        for(char c_dir : t){
+            int dir = (int)(c_dir -'0');
+           
+            for(int i = 0; i < dir; i++){
+                rotate();
+            }
+            for(int i = 0; i < n; i++){
+                 move(board[i]);
+            }
+            for(int i = 0; i < 4 - dir; i++){
+                rotate();
+            }
+        }
+        
+        // 3. 보드에서 가장 큰 블록 찾기
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                mx = max(board[i][j], mx);
+            }
+        }
+    }
+    
+    cout << mx;
+
+    return 0;
 }
