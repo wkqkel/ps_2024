630cfc746bf99bddbdcd2ec0fcbcac006c78eaac
Sangwon Park <wkqkel@naver.com>
2024-06-20 19:15:36 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -1,33 +1,48 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-int n,m;
-int arr[102];
-int cards[102];
-int mx = -1;
+/**
+# BOJ 2798 블랙잭
 
+## 설계
+1. 경우의 수
+최대 100장중 3개뽑기, 순서X, 중복불가 => O(166751) 가능
+
+2. 차이 값 갱신
+보다 클 땐 X, 차이가 기존보다 작으면 sum값 갱신 => O(1)
+*/
+
+const int k = 3;
+int n, m;
+int arr[102];
+int tmp[k];
+int diff = 1e9;
+int res;
 void recur(int cur, int st){
-    if(cur == 3) {
-        int sum = 0;
-        for(int i = 0; i < 3; i++) sum += cards[arr[i]];
-        if(sum <= m && mx < sum) mx = sum;
+    if(cur == k){
+        int sum = arr[tmp[0]] + arr[tmp[1]] + arr[tmp[2]];
+        int ndiff = abs(m - sum);
+        if(sum > m) return;
+        if(diff > ndiff){
+            diff = ndiff;
+            res = sum;
+        }
         return;
-    } 
+    }
     for(int i = st; i < n; i++){
-        arr[cur] = i;
-        recur(cur+1, i+1);
+        tmp[cur] = i;
+        recur(cur+1, i + 1);
     }
 }
 int main()
 {
     cin >> n >> m;
-    
-    for(int i = 0; i <n; i++) cin >> cards[i];
-    
-    recur(0,0);
-    
-    cout << mx;
-    
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    recur(0, 0);
+    cout << res;
+
+    return 0;
 }
