e7516d723edf69ec3bba4af84c44c61ab6cf85d1
Sangwon Park <89131236+wkqkel@users.noreply.github.com>
2024-07-14 23:37:04 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,19 +1,20 @@
 #include <iostream>
-#include <algorithm>
+
 using namespace std;
 
-int arr[10010];
-int d[10010];
+int d[1002];
+
+int mod = 10007;
 int main()
 {
     int n;
     cin >> n;
-    
-    d[1] = 1, d[2] = 2;
+    d[1] = 1;
+    d[2] = 2;
     for(int i = 3; i <= n; i++){
-        d[i] = (d[i-2] + d[i-1]) % 10007;
+        d[i] = (d[i-2] + d[i-1]) % mod;
     }
-   
+    
     cout << d[n];
     return 0;
 }
