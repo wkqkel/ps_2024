bfdd5e90931f731e845cae0bd223c876d4fe5d1f
Sangwon Park <89131236+wkqkel@users.noreply.github.com>
2024-08-22 23:49:51 +0900

    [level 3] Title: 이중우선순위큐, Time: 59.61 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,96 +1,31 @@
 import java.util.*;
 import java.io.*;
 
-class DoublePq {
-    PriorityQueue<Integer> maxPq = new PriorityQueue<>(Collections.reverseOrder());
-    PriorityQueue<Integer> minPq = new PriorityQueue<>();
-    Map<Integer, Integer> cnt = new HashMap<>();
-    int sz = 0;
-    
-    public void push(int v){
-        maxPq.add(v);
-        minPq.add(v);
-        cnt.put(v, cnt.getOrDefault(v, 0) + 1);
-        sz++;
-    }
-    
-    public void popMin(){
-        if(sz == 0) return;
-        while(true){
-            int v = minPq.poll();
-            if(cnt.get(v) > 0){
-                cnt.put(v, cnt.get(v) - 1);
-                break;
-            }
-        }
-        sz--;
-    }
-    
-    public void popMax(){
-        if(sz == 0) return;
-        while(true){
-            int v = maxPq.poll();
-            if(cnt.get(v) > 0){
-                cnt.put(v, cnt.get(v) - 1);
-                break;
-            } 
-        }
-        sz--;
-    }
-    
-    public int getMin(){
-        int ret = 0;
-        if(sz == 0) return ret;
-        while(true){
-            ret = minPq.peek();
-            if(cnt.get(ret) > 0){
-                return ret;
-            } else {
-                minPq.poll();
-            }
-        }
-    }
-    
-    public int getMax(){
-        int ret = 0;
-        if(sz == 0) return ret;
-        while(true){
-            ret = maxPq.peek();
-            if(cnt.get(ret) > 0){
-                return ret;
-            } else {
-                maxPq.poll();
-            }
-        }
-    }
-    
-    public int[] getResult(){
-        return new int[]{getMax(), getMin()};
-    }
-    
-
-}
-
 class Solution {
-    
+    static PriorityQueue<Integer> maxQ = new PriorityQueue<>(Collections.reverseOrder());
+    static PriorityQueue<Integer> minQ = new PriorityQueue<>();
     public int[] solution(String[] ops) {
-        int[] answer = {};
-        DoublePq pq = new DoublePq();
-        
+        StringTokenizer st;
         for(String op : ops){
-           StringTokenizer st = new StringTokenizer(op);
-           String cmd = st.nextToken();
-           int v = Integer.valueOf(st.nextToken());
-    
-           if(cmd.equals("I")){
-               pq.push(v);
-           } else if (v == -1){
-               pq.popMin();
-           } else {
-               pq.popMax();
-           }
+            st = new StringTokenizer(op);
+            String cmd = st.nextToken();
+            int val = Integer.parseInt(st.nextToken());
+ 
+            if("I".equals(cmd)){
+                maxQ.add(val);
+                minQ.add(val);
+            }
+            else if(val == 1){
+                minQ.remove(maxQ.poll());
+            }
+            else {
+                maxQ.remove(minQ.poll());
+            }
         }
         
-        return pq.getResult();
+        if(maxQ.isEmpty()) maxQ.add(0);
+        if(minQ.isEmpty()) minQ.add(0);
+        
+        return new int[]{maxQ.poll(), minQ.poll()};
     }
 }
