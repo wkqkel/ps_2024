bad6db6d1f41652b78f22349698ee0d6e0f24aa4
Sangwon Park <89131236+wkqkel@users.noreply.github.com>
2024-08-27 17:53:11 +0900

    [Gold IV] Title: 치즈, Time: 92 ms, Memory: 12916 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,17 +1,17 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static final int AIR = 0, CHEESE = 1, HOLE = 2;
+	static final int AIR = 0, CHEESE = 1;
 
 	static int R, C;
 	static int board[][] = new int[102][102];
-	static int prev[][] = new int[102][102];
+
 	static int dx[] = new int[] { 1, 0, -1, 0 };
 	static int dy[] = new int[] { 0, 1, 0, -1 };
 
@@ -25,63 +25,29 @@ public class Main {
 			st = new StringTokenizer(br.readLine());
 			for (int c = 0; c < C; c++) {
 				board[r][c] = Integer.parseInt(st.nextToken());
-				prev[r][c] = board[r][c];
+
 			}
 		}
+
 		int time = 0;
+		int cheeseCnt = getCheeseCnt(board);
 		while (true) {
-
-			mark();
-
 			if (getCheeseCnt(board) == 0) {
-				System.out.println(time + "\n" + getCheeseCnt(prev));
 				break;
+			} else {
+				cheeseCnt = getCheeseCnt(board);
 			}
+
 			time++;
 			melt();
 		}
+		System.out.println(time + "\n" + cheeseCnt);
 	}
 
-	public static void mark() {
-		boolean ch[][] = new boolean[102][102];
-		Queue<Pair> q = new ArrayDeque<>();
-
-		for (int r = 0; r < R; r++) {
-			for (int c = 0; c < C; c++) {
-				if (board[r][c] == CHEESE) {
-					continue;
-				}
-
-				board[r][c] = HOLE;
-			}
-		}
-
-		q.add(new Pair(0, 0));
-		ch[0][0] = true;
-
-		while (!q.isEmpty()) {
-			int sz = q.size();
-
-			while (sz-- > 0) {
-				Pair cur = q.poll();
-				board[cur.x][cur.y] = AIR;
-
-				for (int dir = 0; dir < 4; dir++) {
-					int nx = cur.x + dx[dir];
-					int ny = cur.y + dy[dir];
-
-					if (nx < 0 || nx >= R || ny < 0 || ny >= C)
-						continue;
-					if (ch[nx][ny] || board[nx][ny] == CHEESE)
-						continue;
-
-					q.add(new Pair(nx, ny));
-					ch[nx][ny] = true;
-				}
-			}
-		}
-	}
-
+	/**
+	 * @param map
+	 * @return 주어진 이차원배열의 치즈 갯수 반환
+	 */
 	static int getCheeseCnt(int[][] map) {
 		int cheeseCnt = 0;
 		for (int r = 0; r < R; r++) {
@@ -93,36 +59,39 @@ public class Main {
 		return cheeseCnt;
 	}
 
+	/**
+	 * 맵돌면서 공기와 닿은 치즈를 녹임.
+	 */
 	static void melt() {
-		int tmp[][] = new int[102][102];
+		boolean ch[][] = new boolean[102][102];
+		Queue<Pair> q = new ArrayDeque<>();
 
-		for (int r = 0; r < R; r++) {
-			for (int c = 0; c < C; c++) {
-				tmp[r][c] = board[r][c];
-			}
-		}
+		q.add(new Pair(0, 0));
+		ch[0][0] = true;
 
-		for (int r = 0; r < R; r++) {
-			for (int c = 0; c < C; c++) {
-				if (board[r][c] != AIR) {
-					continue;
-				}
+		while (!q.isEmpty()) {
+			int sz = q.size();
+
+			while (sz-- > 0) {
+				Pair cur = q.poll();
 
 				for (int dir = 0; dir < 4; dir++) {
-					int nx = r + dx[dir];
-					int ny = c + dy[dir];
+					int nx = cur.x + dx[dir];
+					int ny = cur.y + dy[dir];
 
 					if (nx < 0 || nx >= R || ny < 0 || ny >= C)
 						continue;
-					tmp[nx][ny] = AIR;
-				}
-			}
-		}
+					if (ch[nx][ny])
+						continue;
+					ch[nx][ny] = true;
+					if (board[nx][ny] == CHEESE) {
+						board[nx][ny] = AIR;
 
-		for (int r = 0; r < R; r++) {
-			for (int c = 0; c < C; c++) {
-				prev[r][c] = board[r][c];
-				board[r][c] = tmp[r][c];
+					} else {
+						q.add(new Pair(nx, ny));
+					}
+
+				}
 			}
 		}
 	}
