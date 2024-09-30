400630986cad9f851534a7eb159cdd0b5605c287
Sangwon Park <89131236+wkqkel@users.noreply.github.com>
2024-09-30 17:39:09 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1148 ms, Memory: 252240 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,43 +1,27 @@
-import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		//System.setIn(new FileInputStream("res/panelsum.txt"));
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		Scanner sc = new Scanner(System.in);
 
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int[] nums = new int[N];
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int psum[] = new int[N + 10];
+		
+		for (int i = 1; i <= N; i++) {
+			psum[i] = psum[i - 1] + sc.nextInt();
+		}
 
-		StringTokenizer st2 = new StringTokenizer(bf.readLine());
-		for (int i = 0; i < N; i++) {
-			int num = Integer.parseInt(st2.nextToken());
-			nums[i] = num;
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i <= M; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			sb.append(psum[b] - psum[a - 1]).append("\n");
 		}
-		
-		for(int i = 1;i < N;i++) {
-			nums[i] = nums[i] + nums[i-1];
-		}
-		
-		for(int i = 0;i < M;i++) {
-			StringTokenizer st3 = new StringTokenizer(bf.readLine());
-			int x = Integer.parseInt(st3.nextToken()) - 2;
-			int y = Integer.parseInt(st3.nextToken()) - 1;
-			
-			if(x >= 0 && y >= 0 && x < nums.length && y < nums.length) {
-				System.out.println(nums[y] - nums[x]);
-			} else {
-				System.out.println(nums[y]);
-			}
-			
-		}
+
+		System.out.println(sb);
 	}
-
 }
