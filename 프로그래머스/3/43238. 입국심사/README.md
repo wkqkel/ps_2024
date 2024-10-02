8846e233f507ba270a082359b5b094a4b8640162
Sangwon Park <89131236+wkqkel@users.noreply.github.com>
2024-10-02 11:08:40 +0900

    [level 3] Title: 입국심사, Time: 45.81 ms, Memory: 6.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.28 ms
+메모리: 6.8 MB, 시간: 45.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 20일 22:57:46
+2024년 10월 02일 11:08:37
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -1,30 +1,32 @@
 #include <string>
+#include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-long getCanProcessingNumber(long t, vector<int> times){
-    long sum = 0;
-    for(int i = 0; i < times.size(); i++){
-        sum += t / times[i];
-    }
-    return sum;
-}
+long long solution(int n, vector<int> times) {
+    long long answer = 0;
+    
+    long long left = 1;
+    long long right = *max_element(times.begin(), times.end()) * (long long) n;
 
-long solution(int n, vector<int> times) {
-    long l = 1;
-    long r = 1e9 * 1e5;
-    
-    while(l <= r){
-        long mid = (l + r) / 2;
-        long pn = getCanProcessingNumber(mid, times);
-        
-        if(n > pn){
-            l = mid + 1;
-        } else {
-            r = mid - 1;
+    while(left <= right)
+    {
+        long long mid = (left+right)/2; long long cnt = 0;
+        for(int i = 0; i < times.size(); i++)
+        {
+            cnt += mid / (long long) times[i];
         }
+        
+        if(cnt >= n)
+        {
+            right = mid - 1;
+            answer = mid;
+        }
+        else left = mid + 1;
     }
     
-    return l;
+    return answer;
+    
 }
