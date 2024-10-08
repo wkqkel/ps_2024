2fadb97e679ae44feda6576e93efbaacb6cb2d3e
Sangwon Park <89131236+wkqkel@users.noreply.github.com>
2024-10-08 09:30:36 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 168 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		long dp[] = new long[102];
+
+		dp[0] = 0;
+		dp[1] = 1;
+		for (int i = 2; i <= N; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+
+		System.out.println(dp[N]);
+	}
+
+}
