8f5c49cccac3245ea46bf468aa805501b22d4c4e
Sangwon Park <89131236+wkqkel@users.noreply.github.com>
2024-10-08 17:08:09 +0900

    [Silver I] Title: 카드 구매하기, Time: 236 ms, Memory: 21180 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-
+		// System.setIn(new FileInputStream("src/input.txt"));
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 
@@ -20,6 +20,8 @@ public class Main {
 		for (int i = 1; i <= N; i++) {
 			dp[i] = arr[i];
 
+			// 현재 고려 중인 i개의 카드를 구하는 모든 경우를
+			// 이미 구한 j개 조합을 통해 갱신 
 			for (int j = 1; j <= i; j++) {
 				dp[i] = Math.max(dp[i], dp[i - j] + dp[j]);
 			}
