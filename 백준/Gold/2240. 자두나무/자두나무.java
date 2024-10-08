232c62cec3ef55bcf1a0aa6ba92ad7dd8cee87ab
Sangwon Park <89131236+wkqkel@users.noreply.github.com>
2024-10-08 11:13:51 +0900

    [Gold V] Title: 자두나무, Time: 100 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.java
+++ 백준/Gold/2240. 자두나무/자두나무.java
@@ -8,48 +8,33 @@ public class Main {
         
         int T = Integer.parseInt(st.nextToken());
         int W = Integer.parseInt(st.nextToken());
-        
-        int[] plums = new int[T + 1];
+        int arr[] = new int[T + 1];
         for (int i = 1; i <= T; i++) {
-            plums[i] = Integer.parseInt(br.readLine());
+            arr[i] = Integer.parseInt(br.readLine());
         }
+        int dp[][][] = new int[T + 1][3][W + 1];
         
-        int[][][] dp = new int[T + 1][3][W + 1];
+        // 초기 상태 설정 - 1번위치로 주어짐.
+        dp[1][1][0] = arr[1] == 1 ? 1 : 0;
+        dp[1][2][1] = arr[1] == 2 ? 1 : 0;
         
-        // 초기 상태 설정
-        if (plums[1] == 1) {
-            dp[1][1][0] = 1;
-        } else {
-            dp[1][2][1] = 1;
-        }
-        
-        for (int t = 2; t <= T; t++) {
-            int currentTree = plums[t];
-            
-            // 이동하지 않는 경우
+        for (int i = 2; i <= T; i++) {
             for (int w = 0; w <= W; w++) {
-                dp[t][1][w] = dp[t-1][1][w];
-                dp[t][2][w] = dp[t-1][2][w];
-                
-                if (currentTree == 1) {
-                    dp[t][1][w]++;
-                } else {
-                    dp[t][2][w]++;
+                int rev = arr[i] == 1 ? 2 : 1;
+                // 이동하지 않는 경우
+                dp[i][arr[i]][w] = dp[i - 1][arr[i]][w] + 1;
+                dp[i][rev][w] = dp[i - 1][rev][w];
+                // 이동하는 경우
+                if (w > 0) {
+                    dp[i][arr[i]][w] = Math.max(dp[i][arr[i]][w], dp[i - 1][rev][w - 1] + 1);
+                    dp[i][rev][w] = Math.max(dp[i][rev][w], dp[i - 1][arr[i]][w - 1]);
                 }
             }
-            
-            // 이동하는 경우
-            for (int w = 1; w <= W; w++) {
-                dp[t][1][w] = Math.max(dp[t][1][w], dp[t-1][2][w-1] + (currentTree == 1 ? 1 : 0));
-                dp[t][2][w] = Math.max(dp[t][2][w], dp[t-1][1][w-1] + (currentTree == 2 ? 1 : 0));
-            }
         }
-        
-        int maxPlums = 0;
+        int ret = 0;
         for (int w = 0; w <= W; w++) {
-            maxPlums = Math.max(maxPlums, Math.max(dp[T][1][w], dp[T][2][w]));
+            ret = Math.max(ret, Math.max(dp[T][1][w], dp[T][2][w]));
         }
-        
-        System.out.println(maxPlums);
+        System.out.println(ret);
     }
 }
