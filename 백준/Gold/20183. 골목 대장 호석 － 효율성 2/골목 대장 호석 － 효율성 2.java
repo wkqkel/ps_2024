f797a2858bba31dbd1f7a292d0a02e641c6a71d9
Sangwon Park <89131236+wkqkel@users.noreply.github.com>
2024-10-22 16:32:41 +0900

    [Gold II] Title: 골목 대장 호석 - 효율성 2, Time: 5492 ms, Memory: 356460 KB, Score: 43 point -BaekjoonHub

--- 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.java
+++ 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.java
@@ -2,21 +2,18 @@ import java.io.*;
 import java.util.*;
 
 /**
-
-1-4로 가는 경로가
-1-(4)->4
-1-(2)->2->(3)->4 
-가 있을 때 C에 따라 갈 수 있을지 말지가 달라짐
-그래서 C미만이면 q에 다 넣어줌.
-그렇게 되면 도착점까지 C이하인 경로만 C에 닿을 수 있고, 과정에서 최대통로비용을 가져감.
-그 후 C에 닿은 경로 중 최솟값을 구함
-
+ * 
+ * 1-4로 가는 경로가 1-(4)->4 1-(2)->2->(3)->4 가 있을 때 C에 따라 갈 수 있을지 말지가 달라짐 그래서 C미만이면
+ * q에 다 넣어줌. 그렇게 되면 도착점까지 C이하인 경로만 C에 닿을 수 있고, 과정에서 최대통로비용을 가져감. 그 후 C에 닿은 경로 중
+ * 최솟값을 구함
+ * 
  */
 public class Main {
 	static final int MX = 100000;
 
 	static int N, M, A, B;
 	static long C;
+	static long INF = Long.MAX_VALUE;
 
 	static long[] dist = new long[MX + 1];
 	static boolean[] ch = new boolean[MX + 1];
@@ -26,62 +23,61 @@ public class Main {
 	static class Edge implements Comparable<Edge> {
 		int v;
 		long d;
-		int mn;
 
-		Edge(int v, long d, int mn) {
+		Edge(int v, long d) {
 			this.v = v;
 			this.d = d;
-			this.mn = mn;
 		}
 
 		@Override
 		public int compareTo(Edge o) {
-
 			return Long.compare(d, o.d);
 		}
 
 		@Override
 		public String toString() {
-			return "Edge [v=" + v + ", d=" + d + ", mn=" + mn + "]";
+			return "Edge [v=" + v + ", d=" + d + "]";
 		}
 
 	}
 
-	public static void getDist(int st) {
+	public static boolean getDist(int mx) {
 		PriorityQueue<Edge> pq = new PriorityQueue<>();
 
 		for (int i = 1; i <= N; i++) {
 			ch[i] = false;
+			dist[i] = INF;
 		}
 
-		pq.add(new Edge(st, 0, 0));
+		pq.add(new Edge(A, 0));
+		dist[A] = 0;
 
 		while (!pq.isEmpty()) {
 			Edge e = pq.poll();
 			int cur = e.v;
 			long d = e.d;
-			int cmn = e.mn;
-			if (cur == B) {
-				ret = Math.min(ret, cmn);
-			}
 
 			if (ch[cur])
 				continue;
 			ch[cur] = true;
-			
+
 			for (int[] edge : edges[cur]) {
 				long nd = edge[0] + d;
 				int nxt = edge[1];
 
-				if (nd <= C) {
-					int nmn = Math.max(cmn, edge[0]);
-					if (cmn == 0)
-						nmn = edge[0];
-					Edge nedge = new Edge(nxt, nd, nmn);
-					pq.add(nedge);
+				if (edge[0] > mx)
+					continue;
+				if (nd > C)
+					continue;
+
+				if (nd < dist[nxt]) {
+					dist[nxt] = nd;
+					pq.add(new Edge(nxt, nd));
 				}
 			}
 		}
+
+		return dist[B] != INF;
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -107,12 +103,19 @@ public class Main {
 			edges[b].add(new int[] { c, a });
 		}
 
-		getDist(A);
-
-		if (ret == Integer.MAX_VALUE) {
-			ret = -1;
+		int s = 1;
+		int e = (int) 1e9;
+		int ret = -1;
+		while (s <= e) {
+			int mid = (s + e) / 2;
+			if (getDist(mid)) {
+				ret = mid;
+				e = mid - 1;
+			} else {
+				s = mid + 1;
+			}
 		}
-
+		
 		System.out.println(ret);
 	}
 }
